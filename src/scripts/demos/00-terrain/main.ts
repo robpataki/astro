ec25a4e8ce73a9026c3054780217f91067047a4a
Rob Pataki <robert@robertpataki.com>
2023-10-11 10:14:39 +0100

    Prettier format files

--- src/scripts/demos/00-terrain/main.ts
+++ src/scripts/demos/00-terrain/main.ts
@@ -1,60 +1,56 @@
-console.log('hello!')
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/terrain.vert';
 
-import fragment from './shaders/fragment.glsl'
-import vertex from './shaders/terrain.vert'
-
-console.log(`fragment: ${fragment}`)
-
-type ShapeObject = THREE.Object3D<THREE.Object3DEventMap>
+type ShapeObject = THREE.Object3D<THREE.Object3DEventMap>;
 
 export default class Terrain {
-  private time = 0
-  private width = 0
-  private height = 0
+  private time = 0;
+  private width = 0;
+  private height = 0;
 
-  private container!: HTMLElement | null
+  private container!: HTMLElement | null;
 
-  private scene!: THREE.Scene
-  private camera!: THREE.PerspectiveCamera
-  private renderer!: THREE.WebGLRenderer
-  protected controls!: any // Where does one get the type for OrbitControls?
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private renderer!: THREE.WebGLRenderer;
+  protected controls!: any; // Where does one get the type for OrbitControls?
 
-  private plane!: ShapeObject
-  private material!: THREE.ShaderMaterial
+  private plane!: ShapeObject;
+  private material!: THREE.ShaderMaterial;
 
   constructor(private options: { container: HTMLElement | null }) {
     if (!this.options.container) {
-      return
+      return;
     }
-    this.container = this.options.container
-    this.width = this.container.offsetWidth
-    this.height = this.container.offsetHeight
+    this.container = this.options.container;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
 
-    this.scene = new THREE.Scene()
-    this.scene.rotation.z = THREE.MathUtils.degToRad(90)
+    this.scene = new THREE.Scene();
+    this.scene.rotation.z = THREE.MathUtils.degToRad(90);
 
-    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10)
-    this.camera.position.z = 1
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10);
+    this.camera.position.z = 1;
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: false,
-    })
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    });
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    this.container.appendChild(this.renderer.domElement)
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.container.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-    this.addObjects()
-    this.handleResize()
-    this.setupEvents()
-    this.render()
+    this.addObjects();
+    this.handleResize();
+    this.setupEvents();
+    this.render();
   }
 
   addObjects() {
-    const geometry = new THREE.PlaneGeometry(4, 4, 100, 100)
+    const geometry = new THREE.PlaneGeometry(4, 12, 100, 300);
     this.material = new THREE.ShaderMaterial({
       uniforms: {
         time: { value: 0 },
@@ -63,38 +59,38 @@ export default class Terrain {
       fragmentShader: fragment,
       vertexShader: vertex,
       wireframe: true,
-    })
-    this.plane = new THREE.Mesh(geometry, this.material)
-    this.plane.rotation.y = THREE.MathUtils.degToRad(45)
-    this.scene.add(this.plane)
+    });
+    this.plane = new THREE.Mesh(geometry, this.material);
+    this.plane.rotation.y = THREE.MathUtils.degToRad(45);
+    this.scene.add(this.plane);
   }
 
   handleResize() {
     if (!this.container) {
-      return
+      return;
     }
 
-    this.width = this.container.offsetWidth
-    this.height = this.container.offsetHeight
-    this.renderer.setSize(this.width, this.height)
-    this.camera.aspect = this.width / this.height
-    this.camera.updateProjectionMatrix()
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
   }
 
   setupEvents() {
-    window.addEventListener('resize', this.handleResize.bind(this))
+    window.addEventListener('resize', this.handleResize.bind(this));
   }
 
   render() {
-    this.time += 0.05
-    this.material.uniforms.time.value = this.time
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
 
-    this.renderer.render(this.scene, this.camera)
+    this.renderer.render(this.scene, this.camera);
 
-    window.requestAnimationFrame(this.render.bind(this))
+    window.requestAnimationFrame(this.render.bind(this));
   }
 }
 
 new Terrain({
   container: document.querySelector('#container'),
-})
+});
--- src/scripts/demos/01-sinus-wave/main.ts
+++ src/scripts/demos/01-sinus-wave/main.ts
@@ -1,64 +1,64 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-type ShapeObject = THREE.Object3D<THREE.Object3DEventMap>
+type ShapeObject = THREE.Object3D<THREE.Object3DEventMap>;
 
 export default class Playground {
-  private time = 0
-  private width = 0
-  private height = 0
+  private time = 0;
+  private width = 0;
+  private height = 0;
 
-  private container!: HTMLElement | null
+  private container!: HTMLElement | null;
 
-  private scene!: THREE.Scene
-  private camera!: THREE.PerspectiveCamera
-  private renderer!: THREE.WebGLRenderer
-  protected controls!: any // Where does one get the type for OrbitControls?
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private renderer!: THREE.WebGLRenderer;
+  protected controls!: any; // Where does one get the type for OrbitControls?
 
-  private uniformData!: any
+  private uniformData!: any;
 
-  private box!: ShapeObject
+  private box!: ShapeObject;
 
   constructor(private options: { container: HTMLElement | null }) {
     if (!this.options.container) {
-      return
+      return;
     }
-    this.container = this.options.container
-    this.width = this.container.offsetWidth
-    this.height = this.container.offsetHeight
+    this.container = this.options.container;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
 
-    this.scene = new THREE.Scene()
-    this.scene.rotation.z = THREE.MathUtils.degToRad(90)
+    this.scene = new THREE.Scene();
+    this.scene.rotation.z = THREE.MathUtils.degToRad(90);
 
-    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 10, 1000)
-    this.camera.position.z = 100
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 10, 1000);
+    this.camera.position.z = 100;
 
-    this.camera.fov = 2 * Math.atan(this.height / 2 / 1000) * (180 / Math.PI)
+    this.camera.fov = 2 * Math.atan(this.height / 2 / 1000) * (180 / Math.PI);
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: true,
-    })
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    });
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    this.container.appendChild(this.renderer.domElement)
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.container.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     this.uniformData = {
       u_time: {
         type: 'f',
         value: this.time,
       },
-    }
+    };
 
-    this.addObjects()
-    this.handleResize()
-    this.setupEvents()
-    this.render()
+    this.addObjects();
+    this.handleResize();
+    this.setupEvents();
+    this.render();
   }
 
   addObjects() {
-    const boxGeometry = new THREE.BoxGeometry(20, 10, 100, 10, 10, 100)
+    const boxGeometry = new THREE.BoxGeometry(20, 10, 100, 10, 10, 100);
     const boxMaterial = new THREE.ShaderMaterial({
       wireframe: true,
       uniforms: this.uniformData,
@@ -81,39 +81,39 @@ export default class Playground {
           * modelViewMatrix
           * result;
       }`,
-    })
-    this.box = new THREE.Mesh(boxGeometry, boxMaterial)
-    this.box.rotation.x = THREE.MathUtils.degToRad(45)
-    this.box.rotation.y = THREE.MathUtils.degToRad(45)
-    this.scene.add(this.box)
+    });
+    this.box = new THREE.Mesh(boxGeometry, boxMaterial);
+    this.box.rotation.x = THREE.MathUtils.degToRad(45);
+    this.box.rotation.y = THREE.MathUtils.degToRad(45);
+    this.scene.add(this.box);
   }
 
   handleResize() {
     if (!this.container) {
-      return
+      return;
     }
 
-    this.width = this.container.offsetWidth
-    this.height = this.container.offsetHeight
-    this.renderer.setSize(this.width, this.height)
-    this.camera.aspect = this.width / this.height
-    this.camera.updateProjectionMatrix()
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
   }
 
   setupEvents() {
-    window.addEventListener('resize', this.handleResize.bind(this))
+    window.addEventListener('resize', this.handleResize.bind(this));
   }
 
   render() {
-    this.time += 0.005
-    this.uniformData.u_time.value = this.time
+    this.time += 0.005;
+    this.uniformData.u_time.value = this.time;
 
-    this.renderer.render(this.scene, this.camera)
+    this.renderer.render(this.scene, this.camera);
 
-    window.requestAnimationFrame(this.render.bind(this))
+    window.requestAnimationFrame(this.render.bind(this));
   }
 }
 
 new Playground({
   container: document.querySelector('#container'),
-})
+});
