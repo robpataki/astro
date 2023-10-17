92dc6d5aa8c0e4cc8b647c7f0621c7f5260d1c20
Rob Pataki <robert@robertpataki.com>
2023-10-17 16:58:54 +0100

    Update title and description

--- src/scripts/const.ts
+++ src/scripts/const.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Rob's tree house";
-export const SITE_DESCRIPTION = 'Welcome to my little online space';
+export const SITE_TITLE = "Rob's code sandbox";
+export const SITE_DESCRIPTION = 'Code experiments around ThreeJS and web accessibility - may even do them both at the same time, who knows!';
 
 export default null;
